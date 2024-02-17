29fc60aba72904b7609b9ef9e454731d89dcde84
mnimp <160103734+mnimp@users.noreply.github.com>
2024-02-17 12:23:22 +0300

    main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from telethon.tl.functions.messages import GetMessagesViewsRequest
 import requests
 from time import sleep
 
-API_ID = '25875948'
-API_HASH = 'bbc8cd4753b320c932bd56254d2917a0'
+API_ID = '23079154'
+API_HASH = 'f269e6ebcc16458ee614a72affcee297'
 user_phone = input("ادخل رقم الهاتف : ")
 client = TelegramClient(user_phone, API_ID, API_HASH)
     
